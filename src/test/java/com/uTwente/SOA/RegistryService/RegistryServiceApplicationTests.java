5378040177b92e816ec0ad8e9ff1010e8d287560
CHAN <charanchan00@gmail.com>
2020-03-16 11:17:22 +0100

    First check in for Registry server

---
+++ src/test/java/com/uTwente/SOA/RegistryService/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uTwente.SOA.RegistryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
